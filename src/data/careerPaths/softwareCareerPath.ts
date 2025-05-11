7477d4bee901143be1421f901b9b3043b30a53cb
amaralc <amaral.calil@gmail.com>
2025-05-11 17:37:57 +0000

    fix: remove default values

--- src/data/careerPaths/softwareCareerPath.ts
+++ src/data/careerPaths/softwareCareerPath.ts
@@ -1,38 +1,11 @@
-
 import { SkillPath } from "../../types/skill";
-import architectureBranch from "./software/architectureBranch";
-import qualityBranch from "./software/qualityBranch";
-import securityBranch from "./software/securityBranch";
-import continuousDeliveryBranch from "./software/continuousDeliveryBranch";
-import frontEndTrack from "./software/frontEndTrack";
-import backEndTrack from "./software/backEndTrack";
-import fullStackTrack from "./software/fullStackTrack";
-import dataScienceTrack from "./software/dataScienceTrack";
-import mobileTrack from "./software/mobileTrack";
-import cloudTrack from "./software/cloudTrack";
-import firmwareTrack from "./software/firmwareTrack";
 import softwareCareerTags from "./software/careerTags";
 
 const softwareCareerPath: SkillPath = {
   id: "software-engineering",
   name: "Engenharia de Software",
   description: "Trilha de desenvolvimento para Engenheiros de Software",
-  branches: [
-    // Base Competencies
-    architectureBranch,
-    qualityBranch,
-    securityBranch,
-    continuousDeliveryBranch,
-    
-    // Specialty Tracks
-    frontEndTrack,
-    backEndTrack,
-    fullStackTrack,
-    dataScienceTrack,
-    mobileTrack,
-    cloudTrack,
-    firmwareTrack,
-  ],
+  branches: [],
   tags: softwareCareerTags,
 };
 
