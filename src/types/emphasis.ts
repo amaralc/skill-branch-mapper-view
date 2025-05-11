8947833347774ef970bd2cffe8c2db983d3985ef
amaralc <amaral.calil@gmail.com>
2025-05-11 16:27:28 +0000

    fix: rename data-science

--- src/types/emphasis.ts
+++ src/types/emphasis.ts
@@ -1,17 +1,41 @@
-
 export type Emphasis = {
   id: string;
   label: string;
   icon: string;
-  group: 'especialidade';
+  group: "especialidade";
 };
 
 export const emphasisOptions: Emphasis[] = [
-  { id: 'front-end', label: 'Front-End', icon: 'front-end', group: 'especialidade' },
-  { id: 'back-end', label: 'Back-End', icon: 'back-end', group: 'especialidade' },
+  {
+    id: "front-end",
+    label: "Front-End",
+    icon: "front-end",
+    group: "especialidade",
+  },
+  {
+    id: "back-end",
+    label: "Back-End",
+    icon: "back-end",
+    group: "especialidade",
+  },
   // Removing Full-Stack as requested
-  { id: 'data-science', label: 'Data Science', icon: 'data-science', group: 'especialidade' },
-  { id: 'mobile', label: 'Mobile', icon: 'mobile', group: 'especialidade' },
-  { id: 'cloud', label: 'Cloud Infrastructure', icon: 'cloud', group: 'especialidade' },
-  { id: 'firmware', label: 'Firmware', icon: 'firmware', group: 'especialidade' }
+  {
+    id: "engineering-and-data-science",
+    label: "Data Science",
+    icon: "engineering-and-data-science",
+    group: "especialidade",
+  },
+  { id: "mobile", label: "Mobile", icon: "mobile", group: "especialidade" },
+  {
+    id: "cloud",
+    label: "Cloud Infrastructure",
+    icon: "cloud",
+    group: "especialidade",
+  },
+  {
+    id: "firmware",
+    label: "Firmware",
+    icon: "firmware",
+    group: "especialidade",
+  },
 ];
